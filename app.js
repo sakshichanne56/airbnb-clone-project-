eae2408c077ee35693e9d016d6413ce22d8c331a
sakshi channe <sakshichanne56@gmail.com>
2026-02-14 15:16:46 +0530

    fixed for render

--- app.js
+++ app.js
@@ -108,12 +108,7 @@ app.use((err,req,res,next)=>{
     // res.status(statusCode).send(message);
 });
 
-// app.listen(8080,()=>{
-//     console.log("server is listening to 8080");
-// });
-
-const port = process.env.PORT || 8080;
-
-app.listen(port, () => {
-    console.log("server is listening to port"` ${port}`);
+app.listen(8080,()=>{
+    console.log("server is listening to 8080");
 });
+
