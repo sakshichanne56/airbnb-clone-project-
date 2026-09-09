4dac1d0e035791f6dc469ce87570076238fcb6c8
sakshi Channe <sakshichanne56@gmail.com>
2026-09-09 14:28:22 +0530

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -4,7 +4,7 @@ module.exports.renderSignupForm=(req,res)=>{
     res.render("users/signup.ejs");
 }
 
-module.exports.signup=async(req,res,next)=>{
+module.exports.signup=async(req,res)=>{
     try{
    let {username,email,password}=req.body;
  const newUser= new User({email,username});
